1d8f9d0d971ac957ffd07bc84080f8883ec42d3d
WenXiang777 <171957027@qq.com>
2022-05-29 23:15:59 +0800

    提交

---
+++ app/src/main/java/com/example/work/UrlAndPassword.java
@@ -0,0 +1,8 @@
+package com.example.work;
+
+public class UrlAndPassword {
+    String url = "jdbc:mysql://101.43.253.15:3306/Android?characterEncoding=utf-8";
+    String password = "root";
+//    String url = "jdbc:mysql://192.168.1.104:3306/android?characterEncoding=utf-8";
+//    String password = "123456";
+}
---
+++ app/src/main/java/com/example/work/successuggestActivity.java
@@ -0,0 +1,19 @@
+package com.example.work;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class successuggestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successuggest);
+    }
+
+    public void close(View view) {
+        finish();
+    }
+}
